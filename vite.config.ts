9e47d4229e2f058bb9857eec9c62cb45f533dc8f
George Cavazos <georgecavazos@Georges-MacBook-Pro.local>
2025-11-28 21:13:11 -0500

    initial project scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react': 'react',
+      'react-dom': 'react-dom',
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'three', '@react-three/fiber', '@react-three/drei'],
+  },
 })
